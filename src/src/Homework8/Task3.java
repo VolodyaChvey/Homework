544557b1ee4359badcee2e62c146513157c3e155
VolodyaChvey <volodya76@tut.by>
2020-12-09 23:09:20 +0300

    homework8

--- src/src/Homework8/Task3.java
+++ src/src/Homework8/Task3.java
@@ -1,4 +1,44 @@
 package src.Homework8;
 
-public class Task3 {
+public final class Task3 {
+    int a;
+    int b;
+    int c;
+
+    public Task3(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        try {
+            check(a, b, c);
+        } catch (NotTriangle e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    static class NotTriangle extends Exception {
+        public NotTriangle(String message) {
+            super(message);
+        }
+    }
+
+    public static void main(String[] args) {
+        Task3 task3 = new Task3(1, 3, 2);
+    }
+
+    void check(int a, int b, int c) throws NotTriangle {
+        if (a > b && a > c) {
+            if (a > (b + c)) {
+                throw new NotTriangle("This is not a triangle");
+            }
+        } else if (b > a && b > c) {
+            if (b > (a + c)) {
+                throw new NotTriangle("This is not a triangle");
+            }
+        } else {
+            if (c > (a + b)) {
+                throw new NotTriangle("This is not a triangle");
+            }
+        }
+    }
 }
